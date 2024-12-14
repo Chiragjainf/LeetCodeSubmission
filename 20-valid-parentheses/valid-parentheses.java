8d591325908fb70094278b647c493f8975315859
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2024-12-14 14:48:30 +0530

    Time: 3 ms (42.84%) | Memory: 42 MB (21.77%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(s.charAt(i) == '(' || s.charAt(i) == '{'  || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } else {
+                char c = s.charAt(i);
+                if(st.isEmpty()) {
+                    return false;
+                }
+                char top = st.peek();
+                st.pop();
+                if(c == ')' && top != '(') {
+                    return false;
+                }
+                if(c == '}' && top != '{') {
+                    return false;
+                }
+                if(c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty() ? true : false;
+        
+    }
+}
