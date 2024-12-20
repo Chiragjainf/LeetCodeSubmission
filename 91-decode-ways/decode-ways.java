a9ffc5ab34afaf3814ee25732d1e6c855ad97e35
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2024-12-20 22:22:02 +0530

    Time: 1 ms (80.96%) | Memory: 42.3 MB (12.04%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -6,32 +6,7 @@ class Solution {
     }
 
 
-    public int helper(String s, Integer dp[]) {
-
-        if(s.length() == 0) {
-            return 1;
-        }
-
-        if(dp[s.length()] != null) {
-            return dp[s.length()];
-        }
-        int one = 0;
-        int two = 0;
-        if(s.length() > 0 && isValid(s.substring(0, 1))) {
-            one = helper(s.substring(1), dp);
-        }
-        if(s.length() > 1 && isValid(s.substring(0, 2))) {
-            two = helper(s.substring(2), dp);
-        }
-
-        return dp[s.length()] = one + two;
-
-    }
-
-
-
-
-    // public int helper(String s) {
+    // public int helper(String s, Integer dp[]) {
 
     //     if(s.length() == 0) {
     //         return 1;
@@ -41,14 +16,42 @@ class Solution {
     //         return dp[s.length()];
     //     }
 
-    //     int cnt = 0;
-    //     for(int i = 1; i <=  2 &&  i <= s.length(); i++) {
-    //         String str = s.substring(0, i);
-    //         if(isValid(str)) {
-    //             cnt = helper(s.substring(i));
-    //         }
+        
+    //     int one = 0;
+    //     int two = 0;
+    //     if(s.length() > 0 && isValid(s.substring(0, 1))) {
+    //         one = helper(s.substring(1), dp);
     //     }
+    //     if(s.length() > 1 && isValid(s.substring(0, 2))) {
+    //         two = helper(s.substring(2), dp);
+    //     }
+
+    //     return dp[s.length()] = one + two;
+
     // }
+
+
+
+
+    public int helper(String s, Integer dp[]) {
+
+        if(s.length() == 0) {
+            return 1;
+        }
+
+        if(dp[s.length()] != null) {
+            return dp[s.length()];
+        }
+
+        int cnt = 0;
+        for(int i = 1; i <=  2 &&  i <= s.length(); i++) {
+            String str = s.substring(0, i);
+            if(isValid(str)) {
+                cnt += helper(s.substring(i), dp);
+            }
+        }
+        return dp[s.length()] = cnt;
+    }
 
     public boolean isValid(String s) {
         if(s.length() >  2) {
