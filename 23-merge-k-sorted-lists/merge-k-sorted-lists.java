c63ec12c9a8c554fb3cd49a68ecf78a4c29c39ef
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-11 09:38:16 +0530

    Time: 2 ms (87.67%) | Memory: 44.8 MB (20.12%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,49 +9,48 @@
  * }
  */
 class Solution {
-
-
-    public ListNode mergeKListsHelper(ListNode[] lists, int l, int r) {
-
-        if(l > r) {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int l = 0,  r = lists.length - 1;
+        if(r < 0) {
             return null;
         }
+        return merge(lists, l , r);
 
+    }
+
+    public ListNode merge(ListNode[] lists, int l, int r) {
+        //System.out.println(l  + " " + r);
         if(l == r) {
             return lists[l];
         }
 
         int mid = l + (r - l) / 2;
-        ListNode list1 = mergeKListsHelper(lists, l , mid);
-        ListNode list2 = mergeKListsHelper(lists, mid + 1 , r);
-        return merge(list1, list2);
-        
+
+        ListNode left = merge(lists, l , mid);
+        ListNode right = merge(lists, mid + 1 , r);
+
+        return mergeTwoLists(left, right);
+
     }
 
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        int l = 0, r = lists.length - 1;
-        return mergeKListsHelper(lists, l ,r);
-    }
 
-    public ListNode merge(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) {
-            return null;
-        } 
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null) {
             return list2;
         }
+
         if(list2 == null) {
             return list1;
         }
 
-        if(list1.val <= list2.val) {
-            list1.next = merge(list1.next,list2);
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
             return list1;
-        }
-        else {
-            list2.next = merge(list1,list2.next);
-            return list2;
+        } else {
+              list2.next = mergeTwoLists(list1, list2.next);
+              return list2;
         }
     }
 }
