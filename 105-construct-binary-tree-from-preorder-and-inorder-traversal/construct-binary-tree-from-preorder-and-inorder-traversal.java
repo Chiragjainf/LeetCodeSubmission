1023413bf03781637669337b0d1df336a5a445fa
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-12 20:41:34 +0530

    Time: 3 ms (34.09%) | Memory: 44.6 MB (41.76%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,45 +14,35 @@
  * }
  */
 class Solution {
-
-    int index = 0;
-
+    int preIndex = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreeHelper(preorder, inorder, 0 , inorder.length - 1);
+        return buildTreeHelper(preorder, inorder, 0, inorder.length - 1);
     }
 
-    public TreeNode buildTreeHelper(int[] preorder, int[] inorder , int l, int r) {
+    public TreeNode buildTreeHelper(int[] preorder, int[] inorder, int l, int r) {
 
         if(l > r) {
             return null;
         }
+        
+        int index = findIndex(preorder[preIndex], inorder, l, r);
 
-        int preIndex = findIndex(inorder, preorder[index]);
+        TreeNode root = new TreeNode(preorder[preIndex++]);
 
+        root.left = buildTreeHelper(preorder, inorder, l , index - 1);
 
-        // if(preIndex == -1) {
-        //     return null; 
-        // }
-
-
-        TreeNode root = new TreeNode(preorder[index++]);
-
-        root.left = buildTreeHelper(preorder, inorder, l , preIndex - 1);
-
-        root.right = buildTreeHelper(preorder, inorder, preIndex + 1 , r);
+        root.right = buildTreeHelper(preorder, inorder, index + 1 , r);
 
         return root;
-        
+
     }
 
-    public int findIndex(int[] inorder, int v) {
-        int n = inorder.length;
-        for(int j = 0; j < n; j++) {
-            if(inorder[j] == v ) {
-                return j;
+    public int findIndex(int val , int[] inorder, int l, int r) {
+        for(int i = l ; i <= r ; i++) {
+            if(inorder[i] == val) {
+                return i;
             }
         }
         return -1;
     }
-
 }
