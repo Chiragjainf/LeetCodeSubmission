d923d02143780b693239f643ab633072e68d7f11
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-12 11:06:18 +0530

    Time: 2 ms (98.11%) | Memory: 44.3 MB (66.21%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,51 +13,30 @@
  *     }
  * }
  */
-
-
-
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(isSameTree(root, subRoot)) {
+            return true;
+        }
 
         if(root == null) {
             return false;
         }
-        Set<String> hs = new HashSet<>();
-        isSameTreeHelper(root, hs);
-        if(hs.contains(isSameTreeHelper2(subRoot))) {
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
             return true;
         }
-        return false;
 
-    }
-
-    public String isSameTreeHelper(TreeNode root, Set<String> hs) {
-
-        if(root == null) {
-            return "N";
+        if(p == null || q == null) {
+            return false;
         }
 
-        String left = isSameTreeHelper(root.left, hs);
-        String right = isSameTreeHelper(root.right, hs);
 
-        hs.add("( " + left + " " + root.val +  " " + right + ")");
-        return "( " + left + " " + root.val +  " " + right + ")";
-        
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-
-    public String isSameTreeHelper2(TreeNode root) {
-
-        if(root == null) {
-            return "N";
-        }
-
-        String left = isSameTreeHelper2(root.left);
-        String right = isSameTreeHelper2(root.right);
-
-        //hs.add(left + " " + root.val +  " " + right);
-        return "( " + left + " " + root.val +  " " + right + ")";
-        
-    }
-
 
 }
