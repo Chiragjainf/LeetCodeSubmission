8c6be10e131e06ad7f24c7535dd72e2e48a9be53
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-15 10:00:46 +0530

    Time: 3 ms (99.84%) | Memory: 45.8 MB (33.99%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,44 +1,64 @@
 class Solution {
 
+
+    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<Integer> adj[] = new List[numCourses];
 
-        for(int i = 0; i < numCourses; i++) {
+        for(int i = 0 ; i < numCourses; i++) {
             adj[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i < prerequisites.length; i++) {
+        int n = prerequisites.length;
+
+        if(n == 0 || numCourses == 1) {
+            return true;
+        }
+
+        for(int i = 0; i < n; i++) {
             adj[prerequisites[i][0]].add(prerequisites[i][1]);
         }
+
 
         boolean vis[] = new boolean[numCourses];
         boolean rec[] = new boolean[numCourses];
 
         for(int i = 0; i < numCourses; i++) {
             if(vis[i] == false) {
-                if(isCycle(adj, i, vis, rec)) {
+                if(cycleExist(adj, vis, rec, i)) {
                     return false;
                 }
             }
         }
-        
+
         return true;
+
     }
 
-    public boolean isCycle(List<Integer> adj[], int src, boolean vis[], boolean rec[]) {
+    public boolean cycleExist(List<Integer> adj[], boolean vis[], boolean rec[], int src) {
 
         vis[src] = true;
+
         rec[src] = true;
 
-        for(int node : adj[src]) {
-            if(vis[node] == false && isCycle(adj, node, vis, rec)) {
+
+        for(int nextNode : adj[src]) {
+            if(vis[nextNode] == false) {
+                if(cycleExist(adj, vis, rec, nextNode)) {
                     return true;
-            } else if(rec[node] == true){
+                }
+            } else if(rec[nextNode] == true) {
                 return true;
             }
         }
 
         rec[src] = false;
+
         return false;
+
     }
+
+
+
+
 }
