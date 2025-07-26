907b1d05c2cc2ec640371da13e6fa86edfc912f7
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-27 02:05:38 +0530

    Time: 43 ms (9.46%) | Memory: 50 MB (5.82%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,38 +1,38 @@
 class Solution {
 
     public int coinChange(int[] coins, int amount) {
-        Integer dp[][] = new Integer[coins.length] [amount + 1];
-        int res = helper(coins, amount, 0, coins.length, 0, dp);
-        return res == 10001 ? -1 : res; 
+        Integer dp[][] = new Integer[coins.length][2 * amount + 1];
+        int res = helper(coins, amount, 0, 0, dp);
+        return res > 10000 ? -1 : res;
     }
 
 
-    public int helper(int[] coins, int amount, int i, int n, int sum, Integer dp[][]) {
+     public int helper(int[] coins, int amount, int currAmount, int index, Integer dp[][]) {
 
-        if( i >= n || sum > amount) {
-            return 10001;
-        }
-
-        if(sum == amount) {
+        if(currAmount == amount) {
             return 0;
         }
 
-        //System.out.println(sum);
-
-        if(dp[i][sum] != null) {
-            return dp[i][sum];
+        if(currAmount > amount) {
+            return 10001;
         }
 
+        if(index >= coins.length) {
+            return 10001;
+        }
+
+        if(dp[index][currAmount] != null) {
+            return dp[index][currAmount];
+        }
 
         int x = 10001;
-        if (coins[i] <= amount ) {
-            x = 1 + helper(coins, amount, i, n, sum + coins[i], dp);
-        }
-        int y = helper(coins, amount, i + 1, n, sum, dp);
 
-        return dp[i][sum] = Math.min(x, y);
+        if(coins[index] <= amount) {
+            x = 1 + helper(coins, amount, currAmount + coins[index], index, dp);
+        }
+        int y = helper(coins, amount, currAmount, index + 1, dp);
+
+        return dp[index][currAmount] = Math.min(x, y);
         
     }
-
-
 }
