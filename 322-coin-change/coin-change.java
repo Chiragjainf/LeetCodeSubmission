a601a63bf2fe1c397d382e0b10faab98c87afead
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-27 02:16:03 +0530

    Time: 15 ms (76.68%) | Memory: 44.8 MB (42.97%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,9 +1,29 @@
 class Solution {
 
     public int coinChange(int[] coins, int amount) {
-        Integer dp[][] = new Integer[coins.length][2 * amount + 1];
-        int res = helper(coins, amount, 0, 0, dp);
-        return res > 10000 ? -1 : res;
+
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, 10001);
+        dp[0] = 0;
+        //System.out.println(dp[0]);
+
+        for(int i = 1; i <= amount; i++) {
+            for(int j = coins.length - 1; j >= 0; j--) {
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                    //System.out.println(dp[i]);
+                }
+
+            }
+        }
+
+        return dp[amount] > 10000 ? -1 : dp[amount];
+
+
+
+        // Integer dp[][] = new Integer[coins.length][2 * amount + 1];
+        // int res = helper(coins, amount, 0, 0, dp);
+        // return res > 10000 ? -1 : res;
     }
 
 
