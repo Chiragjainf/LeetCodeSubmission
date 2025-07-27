edf73bcbc37251eb699ae5e131613eefb4c0d892
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-07-27 11:06:24 +0530

    Time: 431 ms (5.02%) | Memory: 195.5 MB (5.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,28 @@
 class Solution {
-
-
     public int lengthOfLIS(int[] nums) {
-        Integer dp[][] = new Integer[nums.length + 1][nums.length];
-        return helper(nums, -1, 0, nums.length, dp);
+        Integer dp[][] = new Integer[nums.length][nums.length + 1];
+        return helper(nums, 0, -1, nums.length, dp);
     }
 
-    public int helper(int[] nums, int prev , int index, int n, Integer dp[][]) {
+
+     public int helper(int[] nums, int index, int prev, int n,  Integer dp[][]) {
 
         if(index >= n) {
             return 0;
         }
 
-        if(dp[prev + 1][index] != null) {
-            return dp[prev + 1][index];
+        if(dp[index][prev + 1] != null) {
+            return dp[index][prev + 1];
         }
 
+        int x = 0;
+        if(prev == -1 || nums[prev] < nums[index]) {
+            x = 1 + helper(nums, index + 1, index, n, dp);
+        }
 
-        int pick = ((prev == -1 || nums[index] > nums[prev]) ? 1 + helper(nums, index, index + 1, n, dp) : 0);
+        int y = helper(nums, index + 1, prev, n, dp);
 
-        int nonpick = helper(nums, prev, index + 1, n, dp);
-
-        return dp[prev + 1][index] = Math.max(pick , nonpick);
-    
-
+        return dp[index][prev + 1] = Math.max(x, y);
+        
     }
-
-
-
 }
