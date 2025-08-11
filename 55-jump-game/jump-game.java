145f4b13d938d842d96fba474aee4073dd6702a2
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2025-08-11 20:44:46 +0530

    Time: 2 ms (83.29%) | Memory: 45.9 MB (42.94%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int step = nums[0];
-        // if(step == 0 && nums.length > 1) {
-        //     return false;
-        // }
+        int step = nums[0], maxStep = nums[0], res = 1, n = nums.length;
+
+        if(n == 1) {
+            return true;
+        }
+
         for(int i = 1; i < nums.length; i++) {
             if(step <= 0) {
                 return false;
