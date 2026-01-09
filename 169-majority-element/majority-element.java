c555653628ce4f273f19ecef8a60a00530b2b7ee
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2026-01-09 10:28:44 +0530

    Time: 1 ms (99.83%) | Memory: 55.8 MB (33.58%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,14 +3,13 @@ class Solution {
         int me = nums[0], cnt = 1;
 
         for(int i = 1; i < nums.length; i++) {
-            if(me != nums[i]) {
-                cnt--;
-                if(cnt == 0) {
-                    me = nums[i];
-                    cnt = 1;
-                }
-            } else {
+            if(me == nums[i]) {
                 cnt++;
+            }else if(cnt == 0) {
+                me = nums[i];
+                cnt = 1;
+            } else {
+                cnt--;
             }
         }
         return me;
