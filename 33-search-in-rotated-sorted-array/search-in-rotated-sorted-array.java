0360532f48cba8d1eb0cbd00db4ee9f4287879ed
Chiragjainf <43531095+Chiragjainf@users.noreply.github.com>
2026-01-26 15:57:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (36.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,46 +1,31 @@
 class Solution {
+
+
     public int search(int[] nums, int target) {
-        int pivot = findMin(nums);
+        int l = 0, r = nums.length - 1;
 
-        if(nums[0] <= nums[nums.length - 1]) {
-           return binarySearch(nums, 0 , nums.length - 1, target);
-        } else if(target >= nums[0]) {
-            return binarySearch(nums, 0 , pivot - 1, target);
-        } else {
-            return binarySearch(nums, pivot , nums.length - 1, target);
-        }
-    }
-
-    public int binarySearch(int nums[], int l , int r , int target) {
 
         while(l <= r) {
-            int mid = l + (r - l) / 2;
-            //res = Math.min(res, nums[mid]);
+
+            int mid = l + (r -l) / 2;
+
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+            } else if(nums[l] > nums[mid]) {     //second half
+                if (target < nums[mid] || target > nums[r]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {     //first half
+                if (target > nums[mid] || target < nums[l]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
+
         return -1;
-    }
-
-    public int findMin(int[] nums) {
-        
-        int l = 0, r = nums.length - 1, res = nums[0];
-
-        while(l < r) {
-            int mid = l + (r - l) / 2;
-            //res = Math.min(res, nums[mid]);
-            if(nums[mid] < nums[r]) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-
-        return l;
     }
 }
